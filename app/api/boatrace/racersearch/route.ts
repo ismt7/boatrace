415658c290d2da015532448b5f5941d3156fb321
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-01-23 22:41:12 +0900

    Add course trifecta rate extraction and database upsert functionality

--- app/api/boatrace/racersearch/route.ts
+++ app/api/boatrace/racersearch/route.ts
@@ -1,10 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 import { JSDOM } from "jsdom";
+import prisma from "@/app/lib/prismaClient";
+
+export interface CourseTrifectaRate {
+  firstPlaceRate: number;
+  secondPlaceRate: number;
+  thirdPlaceRate: number;
+}
 
 export interface RacerStats {
   racerNumber: string;
   courseAverageStartTiming: number[];
+  courseTrifectaRate: CourseTrifectaRate[];
 }
 
 export async function GET(request: NextRequest) {
@@ -40,8 +48,30 @@ async function fetchRacerStats(toBan: string): Promise<RacerStats> {
   const document = dom.window.document;
 
   const courseAverageStartTiming = extractCourseAverageStartTiming(document);
+  const courseTrifectaRate = extractCourseTrifectaRate(document);
 
-  return { racerNumber: toBan, courseAverageStartTiming };
+  await prisma.racerCourseTrifectaRate.upsert({
+    where: { toban: parseInt(toBan) },
+    update: {
+      firstRate: courseTrifectaRate[0].firstPlaceRate,
+      secondRate: courseTrifectaRate[0].secondPlaceRate,
+      thirdRate: courseTrifectaRate[0].thirdPlaceRate,
+      fourthRate: courseTrifectaRate[1]?.firstPlaceRate || 0,
+      fifthRate: courseTrifectaRate[1]?.secondPlaceRate || 0,
+      sixthRate: courseTrifectaRate[1]?.thirdPlaceRate || 0,
+    },
+    create: {
+      toban: parseInt(toBan),
+      firstRate: courseTrifectaRate[0].firstPlaceRate,
+      secondRate: courseTrifectaRate[0].secondPlaceRate,
+      thirdRate: courseTrifectaRate[0].thirdPlaceRate,
+      fourthRate: courseTrifectaRate[1]?.firstPlaceRate || 0,
+      fifthRate: courseTrifectaRate[1]?.secondPlaceRate || 0,
+      sixthRate: courseTrifectaRate[1]?.thirdPlaceRate || 0,
+    },
+  });
+
+  return { racerNumber: toBan, courseAverageStartTiming, courseTrifectaRate };
 }
 
 function extractCourseAverageStartTiming(document: Document) {
@@ -58,4 +88,75 @@ function extractCourseAverageStartTiming(document: Document) {
   });
 
   return courseAverageStartTiming;
+}
+
+function extractCourseTrifectaRate(document: Document) {
+  const courses = document.querySelectorAll(
+    "body > main > div > div > div > div.contentsFrame1_inner > div > div:nth-child(2) > div.grid.is-type13.h-clear > div:nth-child(1) > div:nth-child(2) > table > tbody:nth-child(n+4):nth-child(-n+9)"
+  );
+
+  if (courses.length === 0) {
+    throw new Error("No data found");
+  }
+
+  const courseTrifectaRate = Array.from(courses).map((element) => {
+    const value = (() => {
+      const textContent = element.querySelector(
+        "tbody > tr > td > div > span"
+      )?.textContent;
+      if (textContent) {
+        const numericValue = textContent.replace("%", "");
+        return parseFloat(numericValue) / 100;
+      }
+      return 0;
+    })();
+
+    const secondPlaceRate = (() => {
+      const percentage = (() => {
+        const style = element
+          .querySelector("tbody > tr > td > div > div > span:nth-child(2)")
+          ?.getAttribute("style");
+
+        if (style) {
+          const widthMatch = style.match(/width:\s*([\d.]+)%/);
+
+          if (widthMatch) {
+            return parseFloat(widthMatch[1]) / 100;
+          }
+        }
+        return 0;
+      })();
+
+      return value * percentage;
+    })();
+
+    const thirdPlaceRate = (() => {
+      const percentage = (() => {
+        const style = element
+          .querySelector("tbody > tr > td > div > div > span:nth-child(3)")
+          ?.getAttribute("style");
+
+        if (style) {
+          const widthMatch = style.match(/width:\s*([\d.]+)%/);
+
+          if (widthMatch) {
+            return parseFloat(widthMatch[1]) / 100;
+          }
+        }
+        return 0;
+      })();
+
+      return value * percentage;
+    })();
+
+    const firstPlaceRate = value - secondPlaceRate - thirdPlaceRate;
+
+    return {
+      firstPlaceRate,
+      secondPlaceRate,
+      thirdPlaceRate,
+    };
+  });
+
+  return courseTrifectaRate;
 }
